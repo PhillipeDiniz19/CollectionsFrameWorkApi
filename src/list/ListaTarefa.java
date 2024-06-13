5e4286672b098ddbaa130e10ac98d0359e6634e8
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-06-13 17:39:19 -0300

    terminando lista tarefa

--- src/list/ListaTarefa.java
+++ src/list/ListaTarefa.java
@@ -30,6 +30,19 @@ public class ListaTarefa {
    }
 
     public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        listaTarefa.adicionarTarefa("Tarefa 3");
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 1");
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricoesTarefas();
 
     }
 }
