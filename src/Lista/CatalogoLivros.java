c4d442fdb5f2a62e05f88b8931b1110584f2f6c4
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-06-17 11:28:41 -0300

    pesquisando listas

---
+++ src/Lista/CatalogoLivros.java
@@ -0,0 +1,16 @@
+package Lista;
+
+public class CatalogoLivros {
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao){
+
+    }
+    public void pesquisarPorAutor(String autor){
+
+    }
+    public void pesquisarPorIntervaloAnos(int anoInicial, int anoFinal){
+
+    }
+    public void pesquisarPorTitulo(String titulo){
+
+    }
+}
