eacf76250d8ee536e38963a3f61f34e5c4572530
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-06-19 12:10:12 -0300

    finalizando aula dio

--- src/Ordenação/OrdenacaoPessoas.java
+++ src/Ordenação/OrdenacaoPessoas.java
@@ -1,6 +1,7 @@
 package Ordenação;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class OrdenacaoPessoas {
@@ -14,9 +15,12 @@ public class OrdenacaoPessoas {
     }
     public List<Pessoa> ordenarPorIdade(){
         List<Pessoa> pessoaPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoaPorIdade);
         return pessoaPorIdade;
     }
-    public void ordenarPorAltura(){
-
+    public List<Pessoa> ordenarPorAltura(){
+        List<Pessoa> pessoaPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoaPorAltura, new ComparatorPorAltura());
+        return pessoaPorAltura;
     }
 }
