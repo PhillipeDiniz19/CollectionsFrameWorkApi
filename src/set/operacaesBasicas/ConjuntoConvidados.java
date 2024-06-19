0f48c71e1f94b9b6f1208ae0e3248aae572e88a7
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-06-19 15:11:53 -0300

    testando codigo

--- src/set/operacaesBasicas/ConjuntoConvidados.java
+++ src/set/operacaesBasicas/ConjuntoConvidados.java
@@ -1,37 +1,55 @@
 package set.operacaesBasicas;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class ConjuntoConvidados {
     private Set<Convidado> convidadoSet;
 
     public ConjuntoConvidados() {
-        this.convidadoSet = convidadoSet;
-    }
-    public void adicionarConvidado(String nome, int codigoConvite){
-        convidadoSet.add(new Convidado(nome, codigoConvite));
-    }
-    public void removerConvidadoPorCodigoConvite(int codigoConvite){
-        Convidado convidadoParaRemover = null;
-        for(Convidado c : convidadoSet){
-            if(c.getCodigoConvite() == codigoConvite){
-                convidadoParaRemover = c;
-                break;
-            }else{
-                System.out.println("Este codigo de convite é invalido.");
-            }
-        }
-        convidadoSet.remove(convidadoParaRemover);
+        this.convidadoSet = new HashSet<>(); // Properly initialize the set
     }
 
-    public int contarConvidados(){
+    public void adicionarConvidado(String nome, int codigoConvite) {
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(int codigoConvite) {
+        Convidado convidadoParaRemover = null;
+        for (Convidado c : convidadoSet) {
+            if (c.getCodigoConvite() == codigoConvite) {
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+        if (convidadoParaRemover != null) {
+            convidadoSet.remove(convidadoParaRemover);
+        } else {
+            System.out.println("Este codigo de convite é invalido.");
+        }
+    }
+
+    public int contarConvidados() {
         return convidadoSet.size();
     }
-    public void exibirConvidados(){
+
+    public void exibirConvidados() {
         System.out.println(convidadoSet);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do set convidados");
+
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 2", 13233);
+        conjuntoConvidados.adicionarConvidado("Convidado 3", 12334);
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do set convidados");
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1234);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " dentro do set convidados");
+
+        conjuntoConvidados.exibirConvidados();
     }
 }
