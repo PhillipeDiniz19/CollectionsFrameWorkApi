225b2a0bee45be4590e7069b4b207312236cee79
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-06-24 11:33:47 -0300

    att: adicionando novas funções

--- src/set/contatos/AgendaContatos.java
+++ src/set/contatos/AgendaContatos.java
@@ -1,4 +1,24 @@
 package set.contatos;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class AgendaContatos {
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos(){
+        this.contatoSet = new HashSet<>();
+    }
+    public void adicionarContato(String nome, int numero){
+        contatoSet.add(new Contato(numero, nome));
+    }
+    public void exibirContatos(){
+
+    }
+    public void pesquisarPorNome(String nome){
+
+    }
+    public void atualizarNumeroContato(String nome, int novoNumero){
+
+    }
 }
