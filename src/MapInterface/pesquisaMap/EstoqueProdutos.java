8727f9338f5a796cca017962bfd48d86d58f925d
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-06-25 12:39:36 -0300

    usando pesquisa com map

--- src/MapInterface/pesquisaMap/EstoqueProdutos.java
+++ src/MapInterface/pesquisaMap/EstoqueProdutos.java
@@ -1,4 +1,90 @@
 package MapInterface.pesquisaMap;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class EstoqueProdutos {
+    private Map<Long, Produto> estoqueProdutosMap;
+
+    public EstoqueProdutos() {
+        this.estoqueProdutosMap = new HashMap<>();
+    }
+    public void adicionarProduto(long cod, String nome, int quantidade, double preco){
+        estoqueProdutosMap.put(cod, new Produto(nome, preco, quantidade));
+    }
+    public void exibirProdutos(){
+        System.out.println(estoqueProdutosMap);
+    }
+    public double calcularValorTotalEstoque(){
+        double valorTotalEstoque = 0d;
+        if(!estoqueProdutosMap.isEmpty()){
+            for(Produto p : estoqueProdutosMap.values()){
+                valorTotalEstoque += p.getQuantidade() * p.getPreco();
+            }
+        }
+        return valorTotalEstoque;
+    }
+    public Produto obterProdutoMaisCaro(){
+        Produto produtoMaisCaro = null;
+        double maiorPreco = Double.MIN_VALUE;
+        if(!estoqueProdutosMap.isEmpty()){
+            for(Produto p: estoqueProdutosMap.values()){
+                if(p.getPreco() > maiorPreco){
+                    produtoMaisCaro = p;
+                }
+            }
+        }
+        return produtoMaisCaro;
+    }
+    public Produto obterProdutoMaisBarato(){
+        Produto produtoMaisBarato = null;
+        double menorPreco = Double.MAX_VALUE;
+        Iterator var4 = this.estoqueProdutosMap.values().iterator();
+
+        while(var4.hasNext()){
+            Produto p = (Produto)var4.next();
+            if(p.getPreco() < menorPreco){
+                produtoMaisBarato = p;
+                menorPreco = p.getPreco();
+            }
+        }
+        return produtoMaisBarato;
+    }
+
+    public Produto obterProdutoMaiorQuantidadeValorTotalNoEstoque() {
+        Produto produtoMaiorQuantidadeValorNoEstoque = null;
+        double maiorValorTotalProdutoEstoque = 0.0;
+        if (!this.estoqueProdutosMap.isEmpty()) {
+            Iterator var4 = this.estoqueProdutosMap.entrySet().iterator();
+
+            while(var4.hasNext()) {
+                Map.Entry<Long, Produto> entry = (Map.Entry)var4.next();
+                double valorProdutoEmEstoque = ((Produto)entry.getValue()).getPreco() * (double)((Produto)entry.getValue()).getQuantidade();
+                if (valorProdutoEmEstoque > maiorValorTotalProdutoEstoque) {
+                    maiorValorTotalProdutoEstoque = valorProdutoEmEstoque;
+                    produtoMaiorQuantidadeValorNoEstoque = (Produto)entry.getValue();
+                }
+            }
+        }
+
+        return produtoMaiorQuantidadeValorNoEstoque;
+    }
+
+    public static void main(String[] args) {
+        EstoqueProdutos estoque = new EstoqueProdutos();
+        estoque.exibirProdutos();
+        estoque.adicionarProduto(1L, "Notebook", 1, 1500.0);
+        estoque.adicionarProduto(2L, "Mouse", 5, 25.0);
+        estoque.adicionarProduto(3L, "Monitor", 10, 400.0);
+        estoque.adicionarProduto(4L, "Teclado", 2, 40.0);
+        estoque.exibirProdutos();
+        System.out.println("Valor total do estoque: R$" + estoque.calcularValorTotalEstoque());
+        Produto produtoMaisCaro = estoque.obterProdutoMaisCaro();
+        System.out.println("Produto mais caro: " + produtoMaisCaro);
+        Produto produtoMaisBarato = estoque.obterProdutoMaisBarato();
+        System.out.println("Produto mais barato: " + produtoMaisBarato);
+        Produto produtoMaiorQuantidadeValorTotal = estoque.obterProdutoMaiorQuantidadeValorTotalNoEstoque();
+        System.out.println("Produto com maior quantidade em valor no estoque: " + produtoMaiorQuantidadeValorTotal);
+    }
 }
