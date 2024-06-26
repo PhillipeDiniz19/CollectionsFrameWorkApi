c04fc729748f8c88c31ba038203c67ebc1137027
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-06-26 11:12:03 -0300

    terminando curso de collections

--- src/MapInterface/ordenacao/AgendaEventos.java
+++ src/MapInterface/ordenacao/AgendaEventos.java
@@ -10,45 +10,42 @@ import java.time.LocalDate;
 public class AgendaEventos {
     private Map<LocalDate, Evento> eventoMap;
 
-    public AgendaEventos(){
+    public AgendaEventos() {
         this.eventoMap = new HashMap<>();
     }
-    public void adicionarEvento(LocalDate data, String nome, String atracao){
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao) {
         Evento evento = new Evento(nome, atracao);
         eventoMap.put(data, evento);
     }
-    public void exibirAgenda(){
-        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>();
-        System.out.println(eventosTreeMap);
+
+    public void exibirAgenda() {
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventoMap);
+        for (Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()) {
+            System.out.println("Data: " + entry.getKey() + " - Evento: " + entry.getValue());
+        }
     }
-    public void obterProximoEvento(){
-        /*Set<LocalDate> dataSet = eventoMap.keySet();
-        Collection<Evento> values = eventoMap.values();
-        eventoMap.get();*/
+
+    public void obterProximoEvento() {
         LocalDate dataAtual = LocalDate.now();
-        LocalDate proximaData = null;
-        Evento proximoEvento = null;
-        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>();
-        for(Map.Entry<LocalDate, Evento> entry : eventoMap.entrySet()){
-            if(entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)){
-                proximaData = entry.getKey();
-                proximoEvento = entry.getValue();
-                System.out.println("O proximo evento: " +  proximoEvento + "acontecerá na data: " + proximaData);
-                break;
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventoMap);
+
+        for (Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()) {
+            if (entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)) {
+                System.out.println("O próximo evento: " + entry.getValue() + " acontecerá na data: " + entry.getKey());
+                return;
             }
         }
-
+        System.out.println("Não há eventos futuros.");
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         AgendaEventos agendaEventos = new AgendaEventos();
         agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), "Evento 1", "Atração 1");
         agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JANUARY, 17), "Evento 2", "Atração 2");
         agendaEventos.adicionarEvento(LocalDate.of(2022, Month.MARCH, 15), "Evento 3", "Atração 3");
-        agendaEventos.adicionarEvento(LocalDate.of(2022, Month.APRIL, 15), "Evento 4", "Atração 4");
+        agendaEventos.adicionarEvento(LocalDate.of(2025, Month.APRIL, 15), "Evento 4", "Atração 4");
         agendaEventos.exibirAgenda();
-
+        agendaEventos.obterProximoEvento();
     }
-
-
 }
