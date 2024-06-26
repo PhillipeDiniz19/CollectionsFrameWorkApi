1db3e03c3d5b0bf033a23598ee3223f4c861b74f
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-06-26 10:39:24 -0300

    new: usando Map, hashMAP, time.localdate

--- src/MapInterface/ordenacao/AgendaEventos.java
+++ src/MapInterface/ordenacao/AgendaEventos.java
@@ -1,4 +1,22 @@
 package MapInterface.ordenacao;
 
+
+import java.util.*;
+import java.time.LocalDate;
+
 public class AgendaEventos {
+    private Map<LocalDate, Evento> eventoMap;
+
+    public AgendaEventos(){
+        this.eventoMap = new HashMap<>();
+    }
+    public void adicionarEvento(LocalDate data, String nome, String atracao){
+        Evento evento = new Evento(nome, atracao);
+        eventoMap.put(data, evento);
+    }
+    public static void main(String[] args){
+        AgendaEventos agendaEventos = new AgendaEventos();
+    }
+
+
 }
